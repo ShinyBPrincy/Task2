d9cd24a7fcd7d69ecb4872b1dde39ce01012aaeb
gayathi694 <gaya63672@gmail.com>
2021-04-09 14:36:50 +0530

    Changes done

--- src/org/test2/GitClass2.java
+++ src/org/test2/GitClass2.java
@@ -13,11 +13,26 @@ public class GitClass2 {
 		System.out.println("test 3");
 
 	}
+	public  void test7() {
+		System.out.println("test 4");
+
+	}
+	public  void test8() {
+		System.out.println("test 5");
+
+	}
+	public  void test9() {
+		System.out.println("test 6");
+
+	}
 	public static void main(String[] args) {
 		GitClass2 t = new GitClass2();
 		t.test1();
 		t.test2();
 		t.test3();
+		t.test7();
+		t.test8();
+		t.test9();
 	}
 
 }
