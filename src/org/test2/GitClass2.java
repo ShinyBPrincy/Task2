b8137e309747cd54bbb50a514442e8774b97577e
amruthasreeg <amruthasreeg1993@gmail.com>
2021-04-09 13:55:16 +0530

    Done

--- src/org/test2/GitClass2.java
+++ src/org/test2/GitClass2.java
@@ -13,11 +13,28 @@ public class GitClass2 {
 		System.out.println("test 3");
 
 	}
+	public  void test4() {
+		System.out.println("test 4");
+
+	}
+	public  void test5() {
+		System.out.println("test 5");
+
+	}
+	public  void test6() {
+		System.out.println("test 6");
+
+	}
+	
 	public static void main(String[] args) {
 		GitClass2 t = new GitClass2();
 		t.test1();
 		t.test2();
 		t.test3();
+		t.test4();
+		t.test5();
+		t.test6();
+		
 	}
 
 }
