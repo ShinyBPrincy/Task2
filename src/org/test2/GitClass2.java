04ef65630e166fd5be5164f736a3ceb8894a41f6
ShinyBPrincy <shinyprincy.jbm@gmail.com>
2021-04-10 00:44:02 +0530

    task done

---
+++ src/org/test2/GitClass2.java
@@ -0,0 +1,23 @@
+package org.test2;
+
+public class GitClass2 {
+	public  void test1() {
+		System.out.println("test 1");
+
+	}
+	public  void test2() {
+		System.out.println("test 2");
+
+	}
+	public  void test3() {
+		System.out.println("test 3");
+
+	}
+	public static void main(String[] args) {
+		GitClass2 t = new GitClass2();
+		t.test1();
+		t.test2();
+		t.test3();
+	}
+
+}
